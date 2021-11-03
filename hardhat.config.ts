52777724db6a8c5cb2aeb7f5028632b294728e79
sergerad <serge.radinovich@immutable.com>
2021-11-03 19:28:26 +1300

    Rm mainnet config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,10 +19,6 @@ module.exports = {
       url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [`0x${process.env.DEPLOYER_ROPSTEN_PRIVATE_KEY}`],
     },
-    mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`0x${process.env.DEPLOYER_MAINNET_PRIVATE_KEY}`],
-    },
   },
   typechain: {
     outDir: "artifacts/typechain",
